b06a87227cff6c26f7a92dd2020c60b8fa569d61
LuchoDelArco <47138072@est.ort.edu.ar>
2022-05-11 19:26:56 -0300

    TakeWhileCondition

--- Assets/Scripts/Instantiator.cs
+++ Assets/Scripts/Instantiator.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Instantiator : MonoBehaviour
 {
     public GameObject objectToClone;
 	public int cloneAmount;
+	public Text txtUserCloneAmount;
 
     public void MultiInstantiate()
     {
         int counter = 0;
+		cloneAmount = int.Parse(txtUserCloneAmount.text);
+		if (txtUserCloneAmount.text != "")
+		{
+			cloneAmount = int.Parse(txtUserCloneAmount.text);
+		}
+		
         while (counter < cloneAmount)
         {
             Instantiate(objectToClone);
